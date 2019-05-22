ab9d68fd1cbfe18bbb1615190345d70d523a31c4
Ben Stanfield <ben@skipper.ai>
2019-05-21 23:20:49 -0700

    citations are working

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   var $ = require('jquery');
   $('.nav-fixed').hide()
 
+  const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
   // Navbar scroll
   if ($('.background-hero-image').length > 0) {
     $(document).scroll(function () {
@@ -35,13 +40,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
   
-
   // Dynamic tooltips?
   let tooltips = {
     carbon_sequestration: {
-    tippyContent: `A natural or artificial process by which carbon dioxide is removed from the atmosphere.`,
-    text: 'carbon sequestration',
-    type: 'noun'
+      tippyContent: `A natural or artificial process by which carbon dioxide is removed from the atmosphere.`,
+      text: 'carbon sequestration',
+      type: 'noun'
     },
     carbon_footprint: {
       tippyContent: `The amount of carbon dioxide and other compounds emitted by the consumption of fossil fuels by a person or group.`,
@@ -50,10 +54,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  const capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
+  let footnotes = {
+    test: {
+      tippyContent: 'We are all students from the University of Southern California <br><a class="citation-link" href="references.html#test">Read more...</a>',
+    },
+    tapingo: {
+      tippyContent: `Phenomenal application <br><a class="citation-link" href="references.html#tapingo">Read more...</a>`,
+    }
   }
+
+  const createFootnote = (ele) => {
+    let classList = $(ele).attr('class').split(/\s+/);
+    classList = classList.filter(x => x != 'tippy');
+    classList = classList.filter(x => x != 'skipper-theme');
+
+    console.log('classList[1]: ', classList[1]);
+    const tippy = footnotes[classList[1]].tippyContent;
+    console.log('tippy: ', footnotes[classList[1]].tippyContent);
+    const num = classList[2];
+    $(`.${classList[1]}`).replaceWith(`<span class='tippy skipper-theme' data-tippy-content='${tippy}' tabindex='0'><sup>${num}</sup></span><script> tippy('.tippy', { theme: 'skipper', animation: 'shift-away', animateFill: false, duration: 200, arrow: true, arrowType: 'round', allowHTML: true, trigger: 'click', interactive: 'true' }) </script>`)
+  }
+
+  let divs = document.getElementsByClassName("footnote");
+  Object.entries(divs).map((element) => createFootnote(element[1]));
 
   const replaceTooltip = (name) => {
     const text = $(name).text();
@@ -61,7 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // this will equal some string
     if (typeof tooltips[text] != 'undefined') {
       const obj = tooltips[text];
-      $(name).replaceWith(`<a class='tippy skipper-theme' data-tippy-content='<strong>${capitalize(obj.text)} (${obj.type})</strong><br> ${obj.tippyContent}' tabindex='0'>${obj.text}<sup>?</sup></a><script> tippy('.tippy', { theme: 'skipper', animation: 'shift-away', animateFill: false, duration: 200, arrow: true, arrowType: 'round', allowHTML: true, trigger: 'click mouseenter' }) </script>`)
+      $(name).replaceWith(`<a class='tippy skipper-theme' data-tippy-content='<strong>${capitalize(obj.text)} (${obj.type})</strong><br> ${obj.tippyContent}' tabindex='0'>${obj.text}</a><script> tippy('.tippy', { theme: 'skipper', animation: 'shift-away', animateFill: false, duration: 200, arrow: true, arrowType: 'round', allowHTML: true, trigger: 'click mouseenter' }) </script>`)
     } else {
       $(name).replaceWith(text)
     }
@@ -82,6 +105,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if ($(".tooltip4").text() != '') {
     replaceTooltip('.tooltip4');
+  }
+
+  const url = $(location).attr('href');
+  if (url.includes('references')) {
+    if (url.includes('#')) {
+      const anchor = url.split('#')[1];
+      console.log(anchor);
+      $(`a[name=${anchor}]`).next('.sub').prepend('👉 ');
+    }
   }
 
 });
